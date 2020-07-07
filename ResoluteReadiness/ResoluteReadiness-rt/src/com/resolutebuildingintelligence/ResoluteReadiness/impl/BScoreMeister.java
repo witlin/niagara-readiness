3006c487381f8ba889e0a7892b36be75b9943dd9
Victor Smolinski <vsmolinski@resolutebi.com>
2020-07-07 02:14:30 -0400

    Victor Smolinski

--- ResoluteReadiness/ResoluteReadiness-rt/src/com/resolutebuildingintelligence/ResoluteReadiness/impl/BScoreMeister.java
+++ ResoluteReadiness/ResoluteReadiness-rt/src/com/resolutebuildingintelligence/ResoluteReadiness/impl/BScoreMeister.java
@@ -7,10 +7,24 @@ import javax.baja.naming.BOrd;
 import javax.baja.nre.annotations.NiagaraType;
 import javax.baja.sys.Context;
 import javax.baja.sys.Sys;
+import javax.baja.sys.Type;
 
 @NiagaraType
 
 public class BScoreMeister extends BSimpleJob {
+/*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
+/*@ $com.resolutebuildingintelligence.ResoluteReadiness.impl.BScoreMeister(2979906276)1.0$ @*/
+/* Generated Tue Jul 07 02:12:21 EDT 2020 by Slot-o-Matic (c) Tridium, Inc. 2012 */
+
+////////////////////////////////////////////////////////////////
+// Type
+////////////////////////////////////////////////////////////////
+  
+  @Override
+  public Type getType() { return TYPE; }
+  public static final Type TYPE = Sys.loadType(BScoreMeister.class);
+
+/*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
 
     public static BScoreMeister make() {
         return new BScoreMeister();
